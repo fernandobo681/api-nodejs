4d64807717af54a4cf10c5ef9d609e474b660b0d
fernando_bmx <fernandobo681@gmail.com>
2023-04-12 00:01:32 -0600

    customer route clean -@fernando_bmx

--- index.js
+++ index.js
@@ -16,5 +16,4 @@ app.use('/api', customerRoutes);
 // MongoDB connection and configuration
 connectToDB();
 
-
 app.listen(port, () => console.log('Server started on port' , port));
--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -3,23 +3,24 @@ const CustomerSchema = require('../models/customer.model');
 module.exports.createCustomer = (req, res) => {
   const customer = CustomerSchema(req.body);
   customer
-    .save().then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+    .save()
+    .then((customer) => res.status(201).json(customer))
+    .catch((err) => res.status(400).json({ message: 'Error to create customers', error: err.message }));
 }
 
 module.exports.getAllCustomers = (req, res) => {
   CustomerSchema
     .find()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+    .then((customer) => res.status(200).json(customer))
+    .catch((err) => res.status(404).json({ message: 'No customers found', error: err.message }));
 }
 
 module.exports.getCustomerById = (req, res) => {
   const { id } = req.params;
   CustomerSchema
     .findById(id)
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+    .then((customer) => res.status(200).json(customer))
+    .catch((err) => res.status(404).json({ message: 'No customer found', error: err.message }));
 }
 
 module.exports.updateCustomerById = (req, res) => {
@@ -27,25 +28,21 @@ module.exports.updateCustomerById = (req, res) => {
   const { name, email, phone, password, reward_points, addresses, payment_methods, coordinates } = req.body;
   CustomerSchema
     .findOneAndUpdate({ _id: id }, {
-      $set: {
-        name,
-        email,
-        phone,
-        password,
-        reward_points,
-        addresses,
-        payment_methods,
-        coordinates
-      }
+      $set: { name, email, phone, password, reward_points, addresses, payment_methods, coordinates }
     })
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+    .then(() => {
+      CustomerSchema
+        .findById(id)
+        .then((customer) => res.status(201).json(customer))
+        .catch((err) => res.status(404).json({ message: 'No customer found', error: err.message }));
+    })
+    .catch((err) => res.status(400).json({ message: 'Error to update customers', error: err.message }));
 }
 
 module.exports.deleteCustomerById = (req, res) => {
   const { id } = req.params;
   CustomerSchema
     .findOneAndDelete({ _id: id })
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+    .then((customer) => res.status(201).json(customer))
+    .catch((err) => res.status(400).json({ message: 'Error to delete customers', error: err.message }));
 }
