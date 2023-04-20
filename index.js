c2c7239b3216e1e3b9cdef9f44a7c662bf3841e8
fernando_bmx <fernandobo681@gmail.com>
2023-04-19 23:05:08 -0600

    Cambio de puerto -@fernando_bmx

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const customerRoutes = require('./src/routes/customer.route');
 const connectToDB = require('./src/config/db');
 
